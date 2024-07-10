efb73e2b7b340efcc14d57bd0d9eb84b73a44693
unknown <phamgiabao1862001@gmail.com>
2024-07-10 21:22:43 +0700

    first commit

---
+++ bnic-sport/src/theme/customMenu/index.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { Menu } from 'antd';
+
+export const CustomMenu = styled(Menu)({
+    backgroundColor: 'rgba(8, 8, 8, 0.78)',
+    boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+    backdropFilter: 'blur(6.4px)',
+    webkitBackdropFilter: 'blur(6.4px)',
+    color: '#ffffff',
+});
